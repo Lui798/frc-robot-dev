652641bf31a2746c9b458529c4849e2a17ddfcea
brosius02 <brosius01@gmail.com>
2020-02-06 17:38:51 -0600

    Prep for motor system and make some classes static
    -CodingSyntax

---
+++ src/main/java/frc/robot/enums/ControlMode.java
@@ -0,0 +1,9 @@
+package frc.robot.enums;
+
+public enum ControlMode
+{
+    Intake,
+    Shooter,
+    Arm,
+    Conveyor
+} 
--- src/main/java/frc/robot/subsystems/ObjectDetectionSystem.java
+++ src/main/java/frc/robot/subsystems/ObjectDetectionSystem.java
@@ -4,7 +4,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Debug;
+import frc.robot.util.Debug;
 
 public class ObjectDetectionSystem {
 
